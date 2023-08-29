1957b968e7a9b1e7e7c026da02d4feb6e8461718
zoimy <108027537+zoimy@users.noreply.github.com>
2023-08-29 16:22:36 +0300

    dff

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -3,16 +3,72 @@ import { ISearchManufacturer } from '@/types'
 import { Combobox, Transition } from '@headlessui/react'
 import Image from 'next/image'
 import '../app/global.css'
-
+import { useState, Fragment} from 'react'
+import { manufacturers } from '@/constants'
 
 const SearchManufacturer = ({manufacturer, setManufacturer}: ISearchManufacturer) => {
+const [query, setQuery] = useState('')
+
+const filteredManufacturers = query === '' ? manufacturer : manufacturers.filter((item) => (
+  item.toLowerCase()
+  .replace(/\s+/g,"")
+  .includes(query.toLowerCase().replace(/\s+/g, ""))
+))
+
+const manufacturersArray = Array.isArray(filteredManufacturers) ? filteredManufacturers : [filteredManufacturers];
+
   return (
     <div className='search-manufacturer'>
-        <Combobox>
+        <Combobox value={manufacturer} onChange={setManufacturer}>
             <div className='relative w-full visible '>
-                <Combobox.Button className='relative top-[-5px]'>
+                <Combobox.Button className='relative top-[42px]'>
                     <Image src='/car-logo.svg' width={20} height={20} className='ml-4' alt='car logo'/>
                 </Combobox.Button>
+                <Combobox.Input  
+                  className='search-manufacturer__input' 
+                  placeholder='Volkswagen' 
+                  displayValue={(manufacturer: string) => manufacturer}
+                  onChange={(e) => setQuery(e.target.value)}
+                />
+                <Transition
+                  as={Fragment}
+                  leave='transition ease-in duration-100'
+                  leaveFrom='opacity-100'
+                  leaveTo='opacity-0'
+                  afterLeave={() => setQuery('')}
+                >
+                  <Combobox.Options>
+                    {
+                      manufacturersArray.map((item )=> (
+                        <Combobox.Option
+                          key={item}
+                          value={item}
+                          className={({active}) => `relative search-manufacturer__option ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}`}
+                        >
+                          {({selected, active}) => (
+                            <>
+                              <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {item}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          >
+                          </span>
+                        ) : null}
+                            </>
+                          )}
+                        </Combobox.Option>
+                      ))
+                    }
+                  </Combobox.Options>
+                </Transition>
             </div>
         </Combobox>
     </div>
